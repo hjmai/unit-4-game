6f4afa4091314718f7789ae28debcf78ac21d650
Jonathan Mai <h.jmai2791@gmail.com>
2018-05-13 18:30:06 -0700

    Layout finished and click events to move characters into correct sections

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -12,7 +12,7 @@ function createCharacter(id, name, imgSrc, hp, attackPower, counterAttack){
     x.name = name;
     x.img = imgSrc;
     x.hp = hp;
-    x.attackpower = attackPower;
+    x.attackPower = attackPower;
     x.counterAttack = counterAttack
     x.isSelected = false;
     return x;
@@ -22,18 +22,33 @@ function StartGame(){
     obi = createCharacter("obi", "Obi-Wan", "./assets/images/obi.jpeg", 150, 13, 15);
     jarjar = createCharacter("jarjar", "Jar Jar", "./assets/images/jarjar.png", 120, 3, 2);
     darthMaul = createCharacter("darthMaul", "Darth Maul", "./assets/images/darthmaul.png", 180, 12, 20);
-    // darthSidious = createCharacter("Darth Sidious", "./assets/images/darthsidious.jpg", 200, 14, 22);
-    charArray.push(obi, jarjar, darthMaul); // push darthSidious later
+    darthSidious = createCharacter("darthSidious", "Darth Sidious", "./assets/images/darthsidious.jpg", 200, 14, 22);
+    charArray.push(obi, jarjar, darthMaul, darthSidious); // push darthSidious later
 }
 
 $(document).ready(function(){
     StartGame();
     console.log("Game is ready to play!");
+
+    // This creates each of our character
     for(var i = 0; i < charArray.length; i++){
-        // $('.characterSection').append('<div class="character" id="'+charArray[i].id+'"> <h3>'+charArray[i].name+'</h3><img src="'+charArray[i].img+'"><h3>HP: '+charArray[i].hp+'</h3>');
-        
+        $('.characterSection').append(
+            '<div class="character" id="'+charArray[i].id+
+            '"data-attack="'+charArray[i].attackPower+
+            '"data-hp="'+charArray[i].hp+
+            '"data-counter="'+charArray[i].counterAttack+'">'+
+            '<h3>'+charArray[i].name+'</h3><img src="'+charArray[i].img+'"><h3>HP: '+charArray[i].hp+'</h3>');
     }
     $('div.character').on('click', function(){
-        console.log($('div.character').data($(this)));
+        if(!yourCharSelected){
+            yourCharSelected = true;
+            $('.yourHero').append($(this));
+        }
+        else{
+            if(!enemyCharSelected){
+                enemyCharSelected = true;
+                $('.enemyHero').append($(this));
+            }
+        }
     })
 });
