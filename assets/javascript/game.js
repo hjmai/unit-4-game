0d59bd1e97d576486d21c9d9148d1acd9dbb046d
Jonathan Mai <h.jmai2791@gmail.com>
2018-05-13 22:01:11 -0700

    Game finish

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -6,12 +6,13 @@ var enemyChar = null;
 var enemyCharSelected = false;
 var enemiesAvailabke = null;
 
-function createCharacter(id, name, imgSrc, hp, attackPower, counterAttack){
+function createCharacter(id, name, imgSrc, hp, baseAttack, attackPower, counterAttack){
     var x = new Object();
     x.id = id;
     x.name = name;
     x.img = imgSrc;
     x.hp = hp;
+    x.baseAttack = baseAttack;
     x.attackPower = attackPower;
     x.counterAttack = counterAttack
     x.isSelected = false;
@@ -19,19 +20,21 @@ function createCharacter(id, name, imgSrc, hp, attackPower, counterAttack){
 }
 
 function StartGame(){
-    obi = createCharacter("obi", "Obi-Wan", "./assets/images/obi.jpeg", 150, 13, 15);
-    jarjar = createCharacter("jarjar", "Jar Jar", "./assets/images/jarjar.png", 120, 3, 2);
-    darthMaul = createCharacter("darthMaul", "Darth Maul", "./assets/images/darthmaul.png", 180, 12, 20);
-    darthSidious = createCharacter("darthSidious", "Darth Sidious", "./assets/images/darthsidious.jpg", 200, 14, 22);
+    obi = createCharacter("obi", "Obi-Wan", "./assets/images/obi.jpeg", 150, 13, 13, 15);
+    jarjar = createCharacter("jarjar", "Jar Jar", "./assets/images/jarjar.png", 120, 3, 3, 2);
+    darthMaul = createCharacter("darthMaul", "Darth Maul", "./assets/images/darthmaul.png", 180, 12, 12, 20);
+    darthSidious = createCharacter("darthSidious", "Darth Sidious", "./assets/images/darthsidious.jpg", 200, 14, 14, 22);
     charArray.push(obi, jarjar, darthMaul, darthSidious); // push darthSidious later
 }
 
 // Creates our characters and inserts them into html
-function reset(){
+function reset(){    
     for(var i = 0; i < charArray.length; i++){
         $('.characterSection').append(
             // This block defines characters data attributes
             '<div class="character" id="'+charArray[i].id+
+            '"data-name="'+charArray[i].name+
+            '"data-base="'+charArray[i].baseAttack+
             '"data-attack="'+charArray[i].attackPower+
             '"data-hp="'+charArray[i].hp+
             '"data-counter="'+charArray[i].counterAttack+'">'+
@@ -39,8 +42,8 @@ function reset(){
             // This block inserts visible html into the .characterSection
             '<h3>'+charArray[i].name+'</h3>'+
             '<img src="'+charArray[i].img+'">'+
-            '<h3>HP: '+charArray[i].hp+'</h3>'+
-            '<h3>Attack: '+charArray[i].attackPower+'</h3>');
+            '<h3 id="hp">HP: '+charArray[i].hp+'</h3>'+
+            '<h3 id="attack">Attack: '+charArray[i].attackPower+'</h3>');
     }
 }
 
@@ -74,8 +77,46 @@ $(document).ready(function(){
         else if(!enemyCharSelected){
             $('.fightSection>div.col-md-12').text("You need to choose an opponent!");
         }
+
+        // This will actually start the fighting
         else{
-            $('.fightSection>div.col-md-12').html("<h2>Let's Battle!</h2>");
+            // Your Character Stats
+            yourChar = $('.yourHero>.character');
+            yourHp = yourChar.data('hp');
+            yourAttack = yourChar.data('attack');
+            yourBase = yourChar.data('base');
+
+            // Enemy Stats
+            enemyChar = $('.enemyHero>.character');
+            enemyHp = enemyChar.data('hp');
+            enemyAttack = enemyChar.data('counter');
+
+            // Stat Update
+            enemyHp = enemyHp - yourAttack;
+            yourAttack = yourAttack + yourBase;
+            yourHp = yourHp - enemyAttack;
+            yourChar.data('attack', yourAttack);
+            yourChar.data('hp', yourHp);
+            enemyChar.data('hp', enemyHp);
+
+
+
+            // HTML update after stats
+            $('.yourHero>.character>#hp').text('HP: '+yourChar.data('hp'));
+            $('.yourHero>.character>#attack').text('Attack: '+yourChar.data('attack'));
+            $('.enemyHero>.character>#hp').text('HP: '+enemyChar.data('hp'));
+
+            $('#battleText').text(yourChar.data('name')+' did '+yourChar.data('attack')+' to '+ enemyChar.data('name'));
+            $('#battleText').append('\n'+enemyChar.data('name')+' did '+enemyChar.data('attack')+' to '+ yourChar.data('name'));
+
+            if(yourChar.data('hp') <= 0){
+                $('.yourHero').empty();
+                $('#battleText').text('Game Over!')
+            }
+            if(enemyChar.data('hp') <= 0){
+                enemyCharSelected = false;
+                $('.enemyHero').empty();
+            }
         }
     })
 });
